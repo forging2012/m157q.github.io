7c92b0a3391ae15a7b9ebbf90006dee41e9ebfd2
m157q <m157q.tw@gmail.com>
2015-02-03 00:29:33 +0800

    pelican-quickstart

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'm157q'
+SITENAME = u'Just for noting'
+SITEURL = u'https://m157q.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+)
+
+# Social widget
+SOCIAL = (
+    ('GitHub', 'https://github.com/M157q'),
+    ('Twitter', 'https://twitter.com/M157q'),
+    ('Google+', 'https://plus.google.com/u/0/+SYJheng/posts'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
