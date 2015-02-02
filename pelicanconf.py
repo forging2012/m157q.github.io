f413e198b1f6695fd82fe45720baf70fe8895046
m157q <m157q.tw@gmail.com>
2015-02-03 02:43:09 +0800

    add some basic settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,28 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+# Basic settings
 AUTHOR = u'm157q'
 SITENAME = u'Just for noting'
+SITESUBTITLE = u'note everything worth noting for me'
 SITEURL = u'https://m157q.github.io'
-
 PATH = 'content'
-
 TIMEZONE = 'Asia/Taipei'
-
 DEFAULT_LANG = u'en'
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
+DISQUS_SITENAME = "m157q-logdown"
+GOOGLE_ANALYTICS = "UA-45367183-1"
+GITHUB_URL = 'https://github.com/M157q'
+TWITTER_USERNAME = u'M157q'
+
+
+# URLs and Paths
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+STATIC_PATHS = ['images']
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -33,7 +46,5 @@ SOCIAL = (
     ('Google+', 'https://plus.google.com/u/0/+SYJheng/posts'),
 )
 
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# Debug
+LOAD_CONTENT_CACHE = False
