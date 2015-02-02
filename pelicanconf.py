4b7df802f3ebda01631eb2f2fd15cf8e47f1600a
m157q <m157q.tw@gmail.com>
2015-02-03 07:36:07 +0800

    add favicon

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,7 +23,10 @@ ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 PAGE_URL = 'pages/{slug}/'
 PAGE_SAVE_AS = 'pages/{slug}/index.html'
-STATIC_PATHS = ['images']
+STATIC_PATHS = ['images', 'extra/favicon.ico']
+EXTRA_PATH_METADATA = {
+    'extra/favicon.ico': {'path': 'favicon.ico'}
+}
 MENUITEMS = [
     #('home', '/index'),
     ('tags', '/tags'),
