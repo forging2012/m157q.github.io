59527200db065377fc0412b2c6fa09875ebb2363
m157q <m157q.tw@gmail.com>
2015-02-11 05:54:07 +0800

    [conf] modify link format

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,22 +18,37 @@ USE_FOLDER_AS_CATEGORY = False
 
 # URLs and Paths
 ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
-ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'
+
 PAGE_URL = 'pages/{slug}/'
-PAGE_SAVE_AS = 'pages/{slug}/index.html'
+PAGE_SAVE_AS = PAGE_URL + 'index.html'
+
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = TAG_URL + 'index.html'
+
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = CATEGORY_URL + 'index.html'
+
+TAGS_SAVE_AS = 'tags/index.html'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_SAVE_AS = 'archives/index.html'
+
 STATIC_PATHS = ['images', 'extra/favicon.ico']
+
 EXTRA_PATH_METADATA = {
     'extra/favicon.ico': {'path': 'favicon.ico'}
 }
+
 MENUITEMS = [
     #('home', '/index'),
-    ('tags', '/tags'),
-    ('categories', '/categories'),
-    ('archives', '/archives'),
+    ('Categories', '/categories'),
+    ('Archives', '/archives'),
+    ('Tags', '/tags'),
     #('search', 'search'),
     #('authors', 'authors'),
     #('about', 'about'),
 ]
+
 DIRECT_TEMPLATES = (
     'index',
     'tags',
